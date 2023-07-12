ac6a6d9ca0112c80f1544af9e7441a7243a5a9a9
Andrew Porter <haporter@me.com>
2023-07-11 21:24:58 -0600

    Models added

---
+++ StarWarsDirectory/StarWarsDirectory/Affiliation.swift
@@ -0,0 +1,15 @@
+//
+//  Affiliation.swift
+//  StarWarsDirectory
+//
+//  Created by Andrew Porter on 7/11/23.
+//
+
+import Foundation
+
+enum Affiliation: String {
+    case jedi = "JEDI"
+    case resistance = "RESISTANCE"
+    case firstOrder = "FIRST_ORDER"
+    case sith = "SITH"
+}
